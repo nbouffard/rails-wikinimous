2e012848ce1af6bb57d964b00a3ca157ad7c6f77
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-15 22:00:39 +0200

    refactored code to keep DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def new
@@ -18,17 +20,17 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
+
     @article.update(article_params)
     redirect_to root_path
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     redirect_to root_path, status: :see_other
   end
@@ -37,5 +39,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
