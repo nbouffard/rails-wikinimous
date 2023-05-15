065a951160598199bd433b14154a51fcfd46a2e2
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-15 20:44:02 +0200

    add 10 seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times { Article.create(title: Faker::Name.name, content: Faker::Lorem.paragraph) }
